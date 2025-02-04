8bc5103849d298ca443210c73d2fb7aa725c7b86
OH <oh@OHui-iMac.local>
2025-02-04 21:59:13 +0900

    chore: create template project

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: true,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 180,
+  tabWidth: 2,
+};
--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
